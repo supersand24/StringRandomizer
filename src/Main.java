3e125d479dc4e7e5e4cbcb1a44817a7692a26771
supersand24 <supersand24@gmail.com>
2022-06-24 11:21:11 -0400

    Added actual random string

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
+    private final static char[] allowedLowercase = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'};
+    private final static char[] allowedUppercase = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z'};
+    private final static char[] allowedNumbers = {'0','1','2','3','4','5','6','7','8','9'};
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        StringBuilder string= new StringBuilder();
+        int stringLength = 10;
+        for (int i = 0; i < stringLength; i++) {
+            switch (getRandomNum(3)) {
+                case 0 -> string.append(allowedLowercase[getRandomNum(allowedLowercase.length)]);
+                case 1 -> string.append(allowedUppercase[getRandomNum(allowedUppercase.length)]);
+                case 2 -> string.append(allowedNumbers[getRandomNum(allowedNumbers.length)]);
+            }
+        }
+        System.out.println(string);
     }
+
+    private static int getRandomNum(int max) { return ThreadLocalRandom.current().nextInt(0, max); }
 
 }
