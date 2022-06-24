cffbb69dc09366cbb8fd2d64bcd3b9206d901bc2
supersand24 <supersand24@gmail.com>
2022-06-24 12:00:11 -0400

    Added Arguement to execute multiple times

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,29 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
-    private final static char[] allowedLowercase = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'};
-    private final static char[] allowedUppercase = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z'};
-    private final static char[] allowedNumbers = {'0','1','2','3','4','5','6','7','8','9'};
+    private final static char[] allowedLowercase = {'a','b','c','d','e','f','h','j','k','m','n','o','p','r','s','t','x','y','z'};
+    private final static char[] allowedUppercase = {'A','B','C','D','E','F','H','J','K','M','N','P','Q','R','T','X','Y','Z'};
+    private final static char[] allowedNumbers = {'3','4','5','7','8','9'};
+
+    private static int times = 1;
 
     public static void main(String[] args) {
-        System.out.println(generateString());
+        System.out.println("Number of Command Line Arguments = " + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.printf("Command Line Argument %d is %s%n",i,args[i]);
+            switch (args[i]) {
+                case "-times" -> {
+                    times = Integer.parseInt(args[i+1]);
+                    i++;
+                }
+            }
+        }
+
+        for (int i = 0; i < times; i++) {
+            System.out.println(generateString());
+        }
     }
+
 
     private static String generateString() { return generateString(10); }
 
